8bac361c7864f6da76136aa285f2a8917521fab2
amranhakim10 <amranhakim9@gmail.com>
2023-03-28 23:33:08 +0700

    Initial commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include "include/navbar.php";
 // include "include/header.php";
 ?>
 <div class="after-nav"></div>
-<div id="fh5co-started" class="fh5co-section animate-box" style="background-image: url(images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
+<div id="fh5co-started" class="fh5co-section animate-box" style="background-image: url(assets/images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row animate-box" style="margin-bottom: 80px;">
@@ -35,30 +35,129 @@ include "include/navbar.php";
                 </div>
             </div>
         </div>
-        <div class="row animate-box" style="margin-bottom: 80px;" data-animate-effect="fadeInLeft">
+
+        <!-- <div class="row animate-box" style="margin-bottom: 80px;">
             <div class="signature">
                 <div class="col-md-6 text-center fh5co-heading">
                     <div class="d-bl">
                         <div>
-                            <font class="f-biasa">NASI AYAM TANGKAP</font>
+                            <font class="f-biasa">Tanpatapi's</font>
                         </div>
-                        <div style="margin-right: 100px;margin-top: -30px;">
-                            <font class="f-latin">Signature</font>
+                    </div>
+                    <div class="d-bl">
+                        <div>
+                            <p class="graf">
+                                Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
+                                dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
+                                Palitho.
+                                Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
+                                negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
+                                lidah Anda melalui kotak yang kami sajikan.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="price-img">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <a href="#" class="btn btn-block btn-default">Price</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 text-center fh5co-heading">
-                    <p class="graf">
-                        Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
-                        dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
-                        Palitho.
-                        Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
-                        negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
-                        lidah Anda melalui kotak yang kami sajikan.
-                    </p>
+                    <div style="margin: 10px;">
+                        <div style="background-color: #fff;">
+                            <div style="padding: 10px;">
+                                <img src="assets/temp/signature/menu1.JPG" class="img-menu-s" alt="Menu 1">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+
+        <div class="row" style="margin-bottom: 80px;">
+            <div class="signature">
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div class="d-bl-menu">
+                        <div class="row">
+                            <div class="col-sm-8" style="text-align: left;line-height: 30px;">
+                                <font class="f-nama-menu">NASI AYAM TANGKAP</font>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8 text-center fh5co-heading">
+                                <p class="graf-menu">
+                                    Hidangan otentik dari Aceh yang diracik dengan
+                                    bumbu rahasia Chef Palitho, lengkap dengan Telur
+                                    Balado Pedas Manis dan Tumis Kacang Panjang yang
+                                    gurih.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <div class="price-img">
+                                    <a href="#" class="btn btn-block btn-default">Price</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div class="row" id="menu-title">
+                        <div class="col-sm-12">
+                            <div class="mar-mar">
+                                <div style="padding: 10px;">
+                                    <img src="assets/temp/signature/menu1.JPG" class="img-menu-s" alt="Menu 1">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="signature">
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div class="d-bl-menu">
+                        <div class="row">
+                            <div class="col-sm-8" style="text-align: left;line-height: 30px;">
+                                <font class="f-nama-menu">NASI AYAM TANGKAP</font>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8 text-center fh5co-heading">
+                                <p class="graf-menu">
+                                    Hidangan otentik dari Aceh yang diracik dengan
+                                    bumbu rahasia Chef Palitho, lengkap dengan Telur
+                                    Balado Pedas Manis dan Tumis Kacang Panjang yang
+                                    gurih.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-8">
+                                <div class="price-img">
+                                    <a href="#" class="btn btn-block btn-default">Price</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div class="row" id="menu-title">
+                        <div class="col-sm-12">
+                            <div class="mar-mar">
+                                <div style="padding: 10px;">
+                                    <img src="assets/temp/signature/menu1.JPG" class="img-menu-s" alt="Menu 1">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 <?php
