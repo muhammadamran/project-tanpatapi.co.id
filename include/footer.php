10a5b2a3dd9d5a7d8c6239569cdac3c07481e4af
amranhakim10 <amranhakim9@gmail.com>
2023-03-29 08:38:36 +0700

    Initial commit

--- include/footer.php
+++ include/footer.php
@@ -1,5 +1,5 @@
-<footer id="fh5co-footer" role="contentinfo" class="fh5co-section" style="    padding: 5em 0;background-image: url('<?= $footer; ?>');">
-    <div class="container" style="margin-bottom: -40px;">
+<footer id="fh5co-footer" role="contentinfo" class="fh5co-section" style="padding: 1em 0;background-image: url('<?= $footer; ?>');">
+    <!-- <div class="container" style="margin-bottom: -40px;">
         <div class="row">
             <div class="col-md-4 fh5co-widget">
                 <div style="margin-bottom: 20px;">
@@ -61,6 +61,73 @@
                             <?php } else { ?>
                                 2023 - <?= date('Y'); ?>
                             <?php } ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <div class="container">
+        <div class="row animate-box">
+            <div class="signature">
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div style="margin-bottom: 20px;text-align: left;">
+                        <font class="hubungi-kami"><i>Hubungi Kami</i></font>
+                    </div>
+                    <ul class="fh5co-footer-links">
+                        <li>
+                            <div style="display:flex;justify-content:flex-start;align-items:flex-start">
+                                <div>
+                                    <img src="assets/icon/whatsapp.png" width="25px" alt="Whatsapp">
+                                </div>
+                                <div style="margin-left: 10px;">
+                                    <font class="hubungi-kami-list"><i>(+62) <?= $WA; ?></i></font>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div style="display:flex;justify-content:flex-start;align-items:flex-start">
+                                <div>
+                                    <img src="assets/icon/instagram.png" width="25px" alt="Instagram">
+                                </div>
+                                <div style="margin-left: 10px;">
+                                    <font class="hubungi-kami-list"><i><?= $IG; ?></i></font>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div style="display:flex;justify-content:flex-start;align-items:flex-start">
+                                <div>
+                                    <img src="assets/icon/mail.png" width="25px" alt="Mail">
+                                </div>
+                                <div style="margin-left: 10px;">
+                                    <font class="hubungi-kami-list"><i><?= $GM; ?></i></font>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div style="display:flex;justify-content:flex-start;align-items:flex-start">
+                                <div>
+                                    <img src="assets/icon/pin.png" width="25px" alt="Address">
+                                </div>
+                                <div style="margin-left: 10px;">
+                                    <font class="hubungi-kami-list"><i><?= $LO; ?></i></font>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div>
+                        <p class="graf">
+                            Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
+                            dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
+                            Palitho.
+                        </p>
+                        <p class="graf">
+                            Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
+                            negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
+                            lidah Anda melalui kotak yang kami sajikan.
                         </p>
                     </div>
                 </div>
--- index.php
+++ index.php
@@ -24,14 +24,47 @@ include "include/navbar.php";
                     </div>
                 </div>
                 <div class="col-md-6 text-center fh5co-heading">
-                    <p class="graf">
-                        Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
-                        dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
-                        Palitho.
-                        Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
-                        negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
-                        lidah Anda melalui kotak yang kami sajikan.
-                    </p>
+                    <div>
+                        <p class="graf">
+                            Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
+                            dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
+                            Palitho.
+                        </p>
+                        <p class="graf">
+                            Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
+                            negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
+                            lidah Anda melalui kotak yang kami sajikan.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row animate-box" style="margin-bottom: 80px;">
+            <div class="signature">
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div class="d-bl">
+                        <div>
+                            <font class="f-biasa">Tanpatapi's</font>
+                        </div>
+                        <div style="margin-right: 100px;margin-top: -30px;">
+                            <font class="f-latin">Signature</font>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 text-center fh5co-heading">
+                    <div>
+                        <p class="graf">
+                            Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
+                            dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
+                            Palitho.
+                        </p>
+                        <p class="graf">
+                            Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
+                            negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
+                            lidah Anda melalui kotak yang kami sajikan.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -74,7 +107,7 @@ include "include/navbar.php";
                     </div>
                 </div>
             </div>
-        </div> -->
+        </div>
 
         <div class="row" style="margin-bottom: 80px;">
             <div class="signature">
@@ -87,12 +120,14 @@ include "include/navbar.php";
                         </div>
                         <div class="row">
                             <div class="col-sm-8 text-center fh5co-heading">
-                                <p class="graf-menu">
-                                    Hidangan otentik dari Aceh yang diracik dengan
-                                    bumbu rahasia Chef Palitho, lengkap dengan Telur
-                                    Balado Pedas Manis dan Tumis Kacang Panjang yang
-                                    gurih.
-                                </p>
+                                <div>
+                                    <p class="graf-menu">
+                                        Hidangan otentik dari Aceh yang diracik dengan
+                                        bumbu rahasia Chef Palitho, lengkap dengan Telur
+                                        Balado Pedas Manis dan Tumis Kacang Panjang yang
+                                        gurih.
+                                    </p>
+                                </div>
                             </div>
                         </div>
                         <div class="row">
@@ -116,47 +151,7 @@ include "include/navbar.php";
                     </div>
                 </div>
             </div>
-
-            <div class="signature">
-                <div class="col-md-6 text-center fh5co-heading">
-                    <div class="d-bl-menu">
-                        <div class="row">
-                            <div class="col-sm-8" style="text-align: left;line-height: 30px;">
-                                <font class="f-nama-menu">NASI AYAM TANGKAP</font>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-8 text-center fh5co-heading">
-                                <p class="graf-menu">
-                                    Hidangan otentik dari Aceh yang diracik dengan
-                                    bumbu rahasia Chef Palitho, lengkap dengan Telur
-                                    Balado Pedas Manis dan Tumis Kacang Panjang yang
-                                    gurih.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-8">
-                                <div class="price-img">
-                                    <a href="#" class="btn btn-block btn-default">Price</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 text-center fh5co-heading">
-                    <div class="row" id="menu-title">
-                        <div class="col-sm-12">
-                            <div class="mar-mar">
-                                <div style="padding: 10px;">
-                                    <img src="assets/temp/signature/menu1.JPG" class="img-menu-s" alt="Menu 1">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </div> -->
 
     </div>
 </div>
