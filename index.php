e6f39dfe0d920488baa0ebdbbfe95ace2baf67b8
amranhakim10 <amranhakim9@gmail.com>
2023-04-07 22:53:48 +0700

    intial commit

--- index.php
+++ index.php
@@ -12,14 +12,18 @@ include "include/navbar.php";
         <div class="nk-portfolio-single">
             <div class="nk-gap-3 mb-14"></div>
             <!-- start 1 -->
+            <?php
+            $QDataSignature = $db->query("SELECT * FROM tb_signature WHERE status='1'");
+            $RDataSignature = mysqli_fetch_array($QDataSignature);
+            ?>
             <div class="row vertical-gap" id="samakan">
                 <div class="col-lg-4">
                     <div style="display: grid;">
                         <div style="line-height: 30px;">
-                            <font class="nk-portfolio-title display-4" id="aya"><b>Tanpatapi’s</b></font>
+                            <font class="nk-portfolio-title display-4" id="aya"><b><?= $RDataSignature['title']; ?></b></font>
                         </div>
                         <div style="margin-top: -20px;">
-                            <font class="nk-portfolio-title display-4" id="gaya">Signature</font>
+                            <font class="nk-portfolio-title display-4" id="gaya"><?= $RDataSignature['subtitle']; ?></font>
                         </div>
                     </div>
                 </div>
@@ -27,16 +31,7 @@ include "include/navbar.php";
                     <div class="nk-portfolio-info">
                         <div class="nk-portfolio-text">
                             <div class="graph">
-                                <p>
-                                    Tanpatapi's Signature merupakan perpaduan masakan khas Nusantara
-                                    dan Mancanegara dengan bumbu khas melalui tangan ajaib Chef
-                                    Palitho.
-                                </p>
-                                <p>
-                                    Dengan ini, Tanpatapi Kitchen merangkum semua kuliner dari berbagai
-                                    negara dalam sebuah kotak. Tanpatapi Kitchen siap untuk memanjakan
-                                    lidah Anda melalui kotak yang kami sajikan.
-                                </p>
+                                <?= $RDataSignature['description']; ?>
                             </div>
                         </div>
                     </div>
@@ -44,38 +39,43 @@ include "include/navbar.php";
             </div>
             <!-- end 1 -->
             <!-- start 2 -->
-            <div class="nk-gap-4 mt-14"></div>
-            <div class="row vertical-gap" id="samakan">
-                <div class="col-lg-4">
-                    <div>
-                        <div>
-                            <font class="nk-portfolio-title display-4" id="judul-menu"><b>NASI AYAM TANGKAP</b></font>
-                        </div>
-                        <div class="graph" id="detail-menu">
-                            <p>
-                                Hidangan otentik dari Aceh yang diracik dengan
-                                bumbu rahasia Chef Palitho, lengkap dengan Telur
-                                Balado Pedas Manis dan Tumis Kacang Panjang yang
-                                gurih.
-                            </p>
-                        </div>
-                        <div>
-                            <a class="btn btn-block btn-default-web">Rp. 45.000</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="nk-portfolio-info">
-                        <div class="nk-portfolio-text">
+            <?php
+            $dataTable = $db->query("SELECT * FROM tb_signature_menu ORDER BY id DESC");
+            if (mysqli_num_rows($dataTable) > 0) {
+                $no = 0;
+                while ($row = mysqli_fetch_array($dataTable)) {
+                    $no++;
+            ?>
+                    <div class="nk-gap-4 mt-14"></div>
+                    <div class="row vertical-gap" id="samakan">
+                        <div class="col-lg-4">
                             <div>
-                                <div class="bingkai-signature">
-                                    <img src="<?= $menuSignature ?>menu1.jpg" class="img-siganture" alt="">
+                                <div>
+                                    <font class="nk-portfolio-title display-4" id="judul-menu"><b><?= $row['title'] ?></b></font>
+                                </div>
+                                <div class="graph" id="detail-menu">
+                                    <?= $row['description'] ?>
+                                </div>
+                                <div>
+                                    <a class="btn btn-block btn-default-web"><?= hargaRupiah($row['prices']); ?></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="nk-portfolio-info">
+                                <div class="nk-portfolio-text">
+                                    <div>
+                                        <div class="bingkai-signature">
+                                            <img src="<?= $menuSignature ?>menu1.jpg" class="img-siganture" alt="">
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
+                <?php } ?>
+            <?php } else { ?>
+            <?php } ?>
             <!-- end 2 -->
             <div class="nk-gap-4 mt-14"></div>
         </div>
